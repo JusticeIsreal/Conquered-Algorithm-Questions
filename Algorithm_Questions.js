1f8c2c051544606341f6e1dc86b5f2455fff26c9
Justice Isreal <justiceyba@gmail.com>
2022-06-19 04:01:45 +0100

    solved some more algorithms

--- Algorithm_Questions.js
+++ Algorithm_Questions.js
@@ -389,3 +389,87 @@ const qualifiedCustomer = (customers) => {
   return message;
 };
 console.log(qualifiedCustomer(obj));
+
+//  QUESTION 9
+
+//  *switchChar* is a function that helps children in swapping values to return a new word
+// a word is a set of alphabets that obviously must not be a dictionary word
+
+// the children are offered three inputs:
+
+// inputChar: cade
+// actualChar: abcdefgh  (is a set of non-repeating alpha-characters)
+// changeChar: grdwqsdr
+
+// switchChar(inputChar, actualChar, changeChar) => dgwq
+
+
+// the result for this is => "dgwq"
+
+// you are to write a function that takes these values as arguments and return the result only if:
+
+// 1) actualChar and changeChar are of the same length,
+// 2) characters in actualChar are not repeated,
+// 3) the length of inputChar is not longer than the length of actualChar
+// 4) the characters in inputChar are contained in actualChar
+
+// if none of this conditions are met, here are the returned values
+
+// 1) actualChar and changeChar are not the same length returns => "Length mismatch"
+// 2) characters in actualChar are repeated returns => 'Repeated value'
+// 3) the length of inputChar is longer than the length of actualChar returns => -1
+// 4) the characters in inputChar are not contained in actualChar -1
+
+
+// *NOTE:*
+// actualChar is a set of none repeating characters of length (0 to N)
+// inputChar is a set of characters of length (0 to N-1)
+// changeChar is a set of characters of length (length of actualChar)
+// unmet conditions must return values
+
+
+// examples:
+// *switchChar('decagon', 'lmnbvcxzasdfghjktyrueiwoqp', 'zdewsaqxcfrtgvbhyujnmkilop') => "rmacgle*
+// *switchChar('we', 'wkeap', 'mgren')​​​ => "mr"*
+
+function switchChar(inputChar = "", actualChar = "", changeChar = "") {
+  let res = "";
+  //check for conditions and constraints
+  if (actualChar.length != changeChar.length) {
+    return "Length mismatch";
+  }
+  if (actualChar.length > 1 && dup(actualChar)) {
+    return "Repeated value";
+  }
+  if (inputChar.length > actualChar.length || !actualChar.contains(inputChar)) {
+    return -1;
+  }
+  //swap the values
+  for (let i = 0; i < inputChar.length; i++) {
+    let index = actualChar.indexOf(inputChar[i]);
+    // console.log(index);
+    res += changeChar[index];
+    console.log(res);
+  }
+  return res;
+}
+//create custom contains for string properties
+String.prototype.contains = function (str = "") {
+  for (let i = 0; i < str.length; i++) {
+    const el = str[i];
+    if (!this.includes(el)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//checks for duplicate
+function dup(str = "") {
+  const arr = [...new Set(str.split(""))];
+  return !(arr.length == str.length);
+}
+
+// console.log(switchChar("decagon", "lmnbvcxzasdfghjktyrueiwoqp", "zdewsaqxcfrtgvbhyujnmkilop"));
+// console.log(switchChar("we", "wkeap", "mgren"));
+switchChar("we", "wkeap", "mgren");
