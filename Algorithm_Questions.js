39564697662e2882e84ed6592d344e7026c11c0a
Justice Isreal <justiceyba@gmail.com>
2022-07-07 22:47:50 +0100

    solved some more algorithm questions from freeCodeCamp

--- Algorithm_Questions.js
+++ Algorithm_Questions.js
@@ -603,16 +603,13 @@ console.log(convertToF(30));
 
 // Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
-
 function largestOfFour(arr) {
   const results = [];
   for (let i = 0; i < arr.length; i++) {
     let largestNumber = arr[i][0];
-   
+
     for (let j = 1; j < arr[i].length; j++) {
-     
       if (arr[i][j] > largestNumber) {
- 
         largestNumber = arr[i][j];
       }
     }
@@ -621,8 +618,14 @@ function largestOfFour(arr) {
 
   return results;
 }
- console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
-;
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
 
 // QUESTION 15
 
@@ -634,4 +637,202 @@ function confirmEnding(str, target) {
 
   return str.slice(str.length - target.length) === target;
 }
-console.log(confirmEnding("He has to give me a new name", "name"))
+console.log(confirmEnding("He has to give me a new name", "name"));
+
+// QUESTION 16
+
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+  let accumulatedStr = "";
+
+  for (let i = 0; i < num; i++) {
+    accumulatedStr += str;
+  }
+  return accumulatedStr;
+}
+console.log(repeatStringNumTimes("abc", 3));
+
+// QUESTION 17
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending
+
+function truncateString(str, num) {
+  let newstr = str.split("").length;
+  let newstr1 = str.split("");
+
+  console.log(num);
+  let gg = ["..."];
+  if (newstr > num) {
+    let answer = newstr1.splice(0, num).join("") + "...";
+    console.log(answer);
+    return answer;
+  } else {
+    return str;
+  }
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(
+  truncateString(
+    "A-tisket a-tasket A green and yellow basket",
+    "A-tisket a-tasket A green and yellow basket".length
+  )
+);
+
+// QUESTION 18
+
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
+console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
+
+// QUESTION 18
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+function booWho(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(booWho(1));
+
+// QUESTION 18
+
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+function titleCase(str) {
+  let newStr = str.split(" ");
+  let newArr = [];
+  for (let i = 0; i < newStr.length; i++) {
+    let elem = newStr[i];
+    newArr.push(elem[0].toUpperCase() + elem.slice(1).toLowerCase());
+    str = newArr.join(" ");
+  }
+  return str;
+}
+console.log();
+titleCase("I'm a little tea pot");
+
+// QUESTION 19
+
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  arr2.splice(n, 0, ...arr1);
+  console.log(arr2);
+  return arr2;
+}
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+//  OR
+
+function frankenSplic(arr1, arr2, n) {
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
+console.log(frankenSplic([1, 2], ["a", "b"], 1));
+
+// QUESTION 20
+
+// Remove all falsy values from an array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// Hint: Try converting each value to a Boolean.
+
+function bouncer(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) newArray.push(arr[i]);
+  }
+  console.log(newArray);
+  return newArray;
+}
+console.log(bouncer([7, "ate", "", false, 9]));
+
+//  OR
+function bounce(arr) {
+  return arr.filter(Boolean);
+}
+console.log(bounce([7, "ate", "", false, 9]));
+
+// QUESTION 21
+
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort((x, y) => x - y);
+  let hh = arr.indexOf(num);
+  return hh;
+}
+console.log(getIndexToIns([40, 80, 60], 50));
+console.log(getIndexToIns([2, 20, 10], 19));
+
+
+// QUESTION 22
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+console.log(mutation(["Mary", "Army"]));
+//  OR
+function mutatio(arr) {
+  let newArr = arr[0].toLowerCase();
+  let checkArr = arr[1].toLowerCase();
+  if (!newArr.includes(checkArr)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(mutatio(["Mary", "Army"]));
+
+
+// QUESTION 23
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+;
