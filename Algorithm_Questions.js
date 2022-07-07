ebe801078cf1c2c946c8bdcdf819369bca458509
Justice Isreal <justiceyba@gmail.com>
2022-07-07 15:30:14 +0100

    solved some more algorithm from freeCodeCamp

--- Algorithm_Questions.js
+++ Algorithm_Questions.js
@@ -596,3 +596,42 @@ function convertToF(celsius) {
   return fahrenheit;
 }
 console.log(convertToF(30));
+
+// QUESTION 14
+
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+
+function largestOfFour(arr) {
+  const results = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumber = arr[i][0];
+   
+    for (let j = 1; j < arr[i].length; j++) {
+     
+      if (arr[i][j] > largestNumber) {
+ 
+        largestNumber = arr[i][j];
+      }
+    }
+    results[i] = largestNumber;
+  }
+
+  return results;
+}
+ console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
+;
+
+// QUESTION 15
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  return str.slice(str.length - target.length) === target;
+}
+console.log(confirmEnding("He has to give me a new name", "name"))
