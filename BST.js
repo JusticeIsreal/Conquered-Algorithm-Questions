71e5bd0d33ff714cb6f69659941c477170d1e8f2
Justice Isreal <justiceyba@gmail.com>
2022-07-27 18:36:23 +0100

    Binary search tree

---
+++ BST.js
@@ -0,0 +1,63 @@
+// Binary search tree O(log n)
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // to add a new node to the tree
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        }
+        if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+  // to check if a value is in tree
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+let Bst = new BinarySearchTree();
